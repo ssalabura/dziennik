ffce236b75109ed90cc799ba34d429c2bdfc1ca5
Grzegorz Gawryał <grzegorz.gawryal@student.uj.edu.pl>
2019-05-06 09:18:57 +0200

    added grades_teacher check and simple grade value check

--- dziennik.sql
+++ dziennik.sql
@@ -18,15 +18,32 @@ create table subjects (
     name character varying(100)
 );
 
-create table grades (
-    grade numeric(1),
-    id_student numeric(10) references students,
-    subject numeric(10) references subjects,
-    teacher numeric(10) references teachers
-);
-
+--Subjects given teacher can teach
 create table teacher_subjects (
     id_teacher numeric(10) references teachers,
     id_subjects numeric(10) references subjects
 );
 
+create table grades (
+    grade numeric(1) check(grade <= 6),
+    id_student numeric(10) references students,
+    subject numeric(10) references subjects,
+    teacher numeric(10) references teachers
+);
+
+
+
+create or replace function grades_teacher_check()
+returns trigger as $grades_teacher_check$
+declare
+    res int;
+begin
+    select into res count(*) from teacher_subjects where id_teacher = new.teacher and id_subjects = new.subject;
+    if res = 0 then
+        raise exception 'cannot add new grade, given teacher does not teach this subject';
+    end if;
+    return new;
+end;
+$grades_teacher_check$ language plpgsql;
+create trigger grades_teacher_check before insert or update on grades
+for each row execute procedure grades_teacher_check();
